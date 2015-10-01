d5844d7140b69b54ad7e41c79c081cfbe1f08b8e
Ben Posey <brp76@pitt.edu>
2015-10-01 12:15:13 -0400

    Added choice method

--- src/Lab4a.java
+++ src/Lab4a.java
@@ -1,15 +1,44 @@
 import java.util.Scanner;
 
 public class Lab4a {
+	public static int choice = 0, utils = 0;
+	public static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		String name;
-		Scanner sc = new Scanner(System.in);
+		
+		int count = 0;
 		
 		System.out.print("What is your name? > ");
 		name = sc.next();
 		
-		System.out.printf("Your name: %s", name);
+		//Repeat until death
+		do {
+			System.out.println("(1) - Buy people ice cream");
+			System.out.println("(2) - Steal ice cream from people");
+			System.out.println("(3) - Dream about people eating ice cream");
+			System.out.println("(4) - Die");
+			
+			//Function for user to enter choice
+			icecreamChoice(name);
+			
+			System.out.print("How many people? > ");
+			count = sc.nextInt();
+			
+		} while (choice != 4);
+
 		
 	}
-
+	//Accepts one argument and returns one value
+	public static int icecreamChoice(String n){
+		//Prompt user for action
+		System.out.printf("Enter next action of %s > ", n);
+		choice = sc.nextInt();
+		while (choice > 4 || choice < 1) {
+			System.out.printf("Enter next action of %s (1-4)> ", n);
+			choice = sc.nextInt();
+		}
+		return choice;
+	}
+	
 }
